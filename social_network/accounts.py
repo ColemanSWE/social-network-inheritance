92fa3eef65f27bea6575aa9800e4fb15d0a03295
Coleman <colemanrose3@yahoo.com>
2018-10-24 16:24:29 -0500

    RMOTRgram Inheritance Project - Initial Commit

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,24 @@
-
 class User(object):
+
+    # Giving the user class initial properties.
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.following = []
+        self.posts = []
 
+    # Adding the ability for the user to add a post using append.
     def add_post(self, post):
-        pass
+        self.posts.append(post)
 
+    # Ability to refresh the user's timeline using a for loop to gather posts from  followed accounts.
     def get_timeline(self):
-        pass
+        timeline = []
+        for _ in self.following:
+            timeline += _.posts
+        return sorted(timeline, key=lambda p: p.timestamp, reverse=False)
 
+    # Adding the ability to follow using append
     def follow(self, other):
-        pass
+        self.following.append(other)
